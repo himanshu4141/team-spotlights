17ac87719959715df0bc31dbbe42c2ae0ad07331
lei-clearsky <sportzhulei@gmail.com>
2016-11-10 17:56:17 -0500

    Rendering the grid

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app.js';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import TeamSpotlights from './components/teamSpotlights.js';
 import './index.scss';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const App = () => (
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <TeamSpotlights />
+    </MuiThemeProvider>
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
